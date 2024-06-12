e53a60e2f7c870ec269d7bcc21863b9d2803f6ee
riya-ambastha <ambasthariya58@gmail.com>
2024-06-12 10:51:33 +0530

    Update LibraryFunction.cpp

--- FunctionAndPointer/LibraryFunction.cpp
+++ FunctionAndPointer/LibraryFunction.cpp
@@ -3,7 +3,7 @@
 // using namespace std;
 // int main() {
 //   cout<<sqrt(4)<<endl;
-//   cout<<pow(2,3)<<endl;
+//   cout<<pow(2,3)<<endl; 
 //   cout<<max(4,8)<<endl;
 //   cout<<min(8,9)<<endl;
 //   cout<<cbrt(1000)<<endl;
